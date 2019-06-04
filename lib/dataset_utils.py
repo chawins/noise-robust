1e84d1c4948ed97ebaf323c9e20ef03f8ca31baf
Chawin Sitawarin <chawins@berkeley.edu>
2019-06-04 16:00:39 +0000

    add normal dist noise, mnist

---
+++ lib/dataset_utils.py
@@ -0,0 +1,380 @@
+'''
+Dataset and DataLoader adapted from
+https://www.kaggle.com/pinocookie/pytorch-dataset-and-dataloader
+'''
+
+import pickle
+
+import torch
+import torchvision
+import torchvision.transforms as transforms
+from PIL import Image
+from sklearn.model_selection import train_test_split
+from torch.utils.data import Dataset
+from torch.utils.data.sampler import SubsetRandomSampler
+
+import numpy as np
+
+
+def rotate_img(img, rot):
+    if rot == 0:  # 0 degrees rotation
+        return img
+    elif rot == 90:  # 90 degrees rotation
+        return np.flipud(np.transpose(img, (1, 0, 2)))
+    elif rot == 180:  # 90 degrees rotation
+        return np.fliplr(np.flipud(img))
+    elif rot == 270:  # 270 degrees rotation / or -90
+        return np.transpose(np.flipud(img), (1, 0, 2))
+    else:
+        raise ValueError('rotation should be 0, 90, 180, or 270 degrees')
+
+
+class RotateDataset(Dataset):
+
+    def __init__(self, dataset):
+        self.dataset = dataset
+        self.transform = transforms.Compose([
+            transforms.ToTensor()
+        ])
+
+    def __len__(self):
+        return len(self.dataset)
+
+    def __getitem__(self, idx):
+        img = self.dataset[idx]
+        rotated_imgs = [
+            self.transform(img),
+            self.transform(rotate_img(img, 90).copy()),
+            self.transform(rotate_img(img, 180).copy()),
+            self.transform(rotate_img(img, 270).copy())
+        ]
+        rotation_labels = torch.LongTensor([0, 1, 2, 3])
+        return torch.stack(rotated_imgs, dim=0), rotation_labels
+
+
+def load_mnist(batch_size,
+               data_dir='./data',
+               val_size=0.1,
+               shuffle=True,
+               seed=1):
+    """Load MNIST data into train/val/test data loader"""
+
+    num_workers = 4
+
+    (x_train, y_train), (x_valid, y_valid), (x_test, y_test) = load_mnist_all(
+        data_dir=data_dir, val_size=val_size, shuffle=shuffle, seed=seed)
+
+    trainset = torch.utils.data.TensorDataset(x_train, y_train)
+    validset = torch.utils.data.TensorDataset(x_valid, y_valid)
+    testset = torch.utils.data.TensorDataset(x_test, y_test)
+    trainloader = torch.utils.data.DataLoader(
+        trainset, batch_size=batch_size, num_workers=num_workers)
+    validloader = torch.utils.data.DataLoader(
+        validset, batch_size=batch_size, num_workers=num_workers)
+    testloader = torch.utils.data.DataLoader(
+        testset, batch_size=batch_size, shuffle=False, num_workers=num_workers)
+
+    return trainloader, validloader, testloader
+
+
+def load_mnist_all(data_dir='./data', val_size=0.1, shuffle=True, seed=1):
+    """Load entire MNIST dataset into tensor"""
+
+    transform = transforms.Compose([
+        transforms.ToTensor(),
+    ])
+
+    trainset = torchvision.datasets.MNIST(
+        root=data_dir, train=True, download=True, transform=transform)
+    testset = torchvision.datasets.MNIST(
+        root=data_dir, train=False, download=True, transform=transform)
+
+    trainloader = torch.utils.data.DataLoader(
+        trainset, batch_size=len(trainset), shuffle=False)
+    testloader = torch.utils.data.DataLoader(
+        testset, batch_size=len(testset), shuffle=False)
+
+    x, y = next(iter(trainloader))
+    x_test, y_test = next(iter(testloader))
+
+    x_train, x_valid, y_train, y_valid = train_test_split(
+        x.numpy(), y.numpy(), test_size=val_size, shuffle=shuffle,
+        random_state=seed, stratify=y)
+
+    return ((torch.tensor(x_train), torch.tensor(y_train)),
+            (torch.tensor(x_valid), torch.tensor(y_valid)), (x_test, y_test))
+
+
+def load_mnist_rot(batch_size, data_dir='./data', val_size=0.1, shuffle=True,
+                   seed=1):
+
+    (x_train, _), (x_valid, _), (x_test, _) = load_mnist_all(
+        data_dir, val_size=val_size, seed=seed)
+
+    traindataset = RotateDataset(x_train.numpy().transpose(0, 2, 3, 1))
+    trainloader = torch.utils.data.DataLoader(
+        traindataset, batch_size=batch_size, shuffle=shuffle, num_workers=4)
+
+    validdataset = RotateDataset(x_valid.numpy().transpose(0, 2, 3, 1))
+    validloader = torch.utils.data.DataLoader(
+        validdataset, batch_size=batch_size, shuffle=False, num_workers=4)
+
+    testdataset = RotateDataset(x_test.numpy().transpose(0, 2, 3, 1))
+    testloader = torch.utils.data.DataLoader(
+        testdataset, batch_size=batch_size, shuffle=False, num_workers=4)
+
+    return trainloader, validloader, testloader
+
+
+def load_cifar10(batch_size,
+                 data_dir='./data',
+                 val_size=0.1,
+                 normalize=True,
+                 augment=True,
+                 shuffle=True,
+                 seed=1):
+    """Load CIFAR-10 data into train/val/test data loader"""
+
+    mean = (0.4914, 0.4822, 0.4465)
+    std = (0.2023, 0.1994, 0.2010)
+    num_workers = 4
+
+    transform = transforms.Compose([
+        transforms.ToTensor()
+    ])
+
+    if augment:
+        transform_train = transforms.Compose([
+            transforms.RandomCrop(32, padding=4),
+            transforms.RandomHorizontalFlip(),
+            transforms.RandomAffine(
+                5, translate=(0.1, 0.1), scale=(0.9, 1.1), shear=5),
+            transforms.ColorJitter(brightness=0.1),
+            transforms.ToTensor()
+        ])
+    else:
+        transform_train = transform
+
+    if normalize:
+        transform = transforms.Compose([
+            transform,
+            transforms.Normalize(mean, std)
+        ])
+        transform_train = transforms.Compose([
+            transform_train,
+            transforms.Normalize(mean, std)
+        ])
+
+    trainset = torchvision.datasets.CIFAR10(
+        root=data_dir, train=True, download=True, transform=transform_train)
+    validset = torchvision.datasets.CIFAR10(
+        root=data_dir, train=True, download=True, transform=transform)
+    testset = torchvision.datasets.CIFAR10(
+        root=data_dir, train=False, download=True, transform=transform)
+
+    # Random split train and validation sets
+    num_train = len(trainset)
+    indices = list(range(num_train))
+    split = int(np.floor(val_size * num_train))
+
+    if shuffle:
+        np.random.seed(seed)
+        np.random.shuffle(indices)
+
+    train_idx, valid_idx = indices[split:], indices[:split]
+    train_sampler = SubsetRandomSampler(train_idx)
+    valid_sampler = SubsetRandomSampler(valid_idx)
+
+    trainloader = torch.utils.data.DataLoader(
+        trainset, batch_size=batch_size, sampler=train_sampler,
+        num_workers=num_workers)
+    validloader = torch.utils.data.DataLoader(
+        validset, batch_size=batch_size, sampler=valid_sampler,
+        num_workers=num_workers)
+    testloader = torch.utils.data.DataLoader(
+        testset, batch_size=batch_size, shuffle=False, num_workers=num_workers)
+
+    return trainloader, validloader, testloader
+
+
+def load_cifar10_all(data_dir='./data', val_size=0.1, shuffle=True, seed=1):
+    """Load entire CIFAR-10 dataset into tensor"""
+
+    transform = transforms.Compose([
+        transforms.ToTensor(),
+    ])
+
+    trainset = torchvision.datasets.CIFAR10(
+        root=data_dir, train=True, download=True, transform=transform)
+    validset = torchvision.datasets.CIFAR10(
+        root=data_dir, train=True, download=True, transform=transform)
+    testset = torchvision.datasets.CIFAR10(
+        root=data_dir, train=False, download=True, transform=transform)
+
+    # Random split train and validation sets
+    num_train = len(trainset)
+    indices = list(range(num_train))
+    split = int(np.floor(val_size * num_train))
+
+    if shuffle:
+        np.random.seed(seed)
+        np.random.shuffle(indices)
+
+    train_idx, valid_idx = indices[split:], indices[:split]
+    train_sampler = SubsetRandomSampler(train_idx)
+    valid_sampler = SubsetRandomSampler(valid_idx)
+
+    trainloader = torch.utils.data.DataLoader(
+        trainset, batch_size=(num_train - split), sampler=train_sampler)
+    validloader = torch.utils.data.DataLoader(
+        validset, batch_size=split, sampler=valid_sampler)
+    testloader = torch.utils.data.DataLoader(
+        testset, batch_size=len(testset), shuffle=False)
+
+    x_train = next(iter(trainloader))
+    x_valid = next(iter(validloader))
+    x_test = next(iter(testloader))
+
+    return x_train, x_valid, x_test
+
+
+def load_cifar10_rot(batch_size, data_dir='./data', val_size=0.1, shuffle=True,
+                     seed=1):
+
+    (x_train, _), (x_valid, _), (x_test, _) = load_cifar10_all(
+        data_dir, val_size=val_size, seed=seed)
+
+    traindataset = RotateDataset(x_train.numpy().transpose(0, 2, 3, 1))
+    trainloader = torch.utils.data.DataLoader(
+        traindataset, batch_size=batch_size, shuffle=shuffle, num_workers=4)
+
+    validdataset = RotateDataset(x_valid.numpy().transpose(0, 2, 3, 1))
+    validloader = torch.utils.data.DataLoader(
+        validdataset, batch_size=batch_size, shuffle=False, num_workers=4)
+
+    testdataset = RotateDataset(x_test.numpy().transpose(0, 2, 3, 1))
+    testloader = torch.utils.data.DataLoader(
+        testdataset, batch_size=batch_size, shuffle=False, num_workers=4)
+
+    return trainloader, validloader, testloader
+
+
+def load_gtsrb(data_dir='./data', gray=False, train_file_name=None):
+    """
+    Load GTSRB data as a (datasize) x (channels) x (height) x (width) numpy
+    matrix. Each pixel is rescaled to lie in [0,1].
+    """
+
+    def load_pickled_data(file, columns):
+        """
+        Loads pickled training and test data.
+
+        Parameters
+        ----------
+        file    : string
+                          Name of the pickle file.
+        columns : list of strings
+                          List of columns in pickled data we're interested in.
+
+        Returns
+        -------
+        A tuple of datasets for given columns.
+        """
+
+        with open(file, mode='rb') as f:
+            dataset = pickle.load(f)
+        return tuple(map(lambda c: dataset[c], columns))
+
+    def preprocess(x, gray):
+        """
+        Preprocess dataset: turn images into grayscale if specified, normalize
+        input space to [0,1], reshape array to appropriate shape for NN model
+        """
+
+        if not gray:
+            # Scale features to be in [0, 1]
+            x = (x / 255.).astype(np.float32)
+        else:
+            # Convert to grayscale, e.g. single Y channel
+            x = 0.299 * x[:, :, :, 0] + 0.587 * x[:, :, :, 1] + \
+                0.114 * x[:, :, :, 2]
+            # Scale features to be in [0, 1]
+            x = (x / 255.).astype(np.float32)
+            x = x[:, :, :, np.newaxis]
+        return x
+
+    # Load pickle dataset
+    if train_file_name is None:
+        x_train, y_train = load_pickled_data(
+            data_dir + 'train.p', ['features', 'labels'])
+    else:
+        x_train, y_train = load_pickled_data(
+            data_dir + train_file_name, ['features', 'labels'])
+    x_val, y_val = load_pickled_data(
+        data_dir + 'valid.p', ['features', 'labels'])
+    x_test, y_test = load_pickled_data(
+        data_dir + 'test.p', ['features', 'labels'])
+
+    # Preprocess loaded data
+    x_train = preprocess(x_train, gray)
+    x_val = preprocess(x_val, gray)
+    x_test = preprocess(x_test, gray)
+    return x_train, y_train, x_val, y_val, x_test, y_test
+
+
+class GtsrbDataset(torch.utils.data.Dataset):
+
+    def __init__(self, x_np, y_np, mean=None, std=None, augment=False):
+
+        self.x_pil = [Image.fromarray(
+            (x * 255).astype(np.uint8)) for x in x_np]
+        self.y_np = y_np.astype(np.int64)
+
+        if mean is None:
+            mean = (0, 0, 0)
+            std = (1, 1, 1)
+
+        if augment:
+            self.transform = transforms.Compose([
+                transforms.RandomCrop(32, padding=4, padding_mode='edge'),
+                transforms.RandomAffine(
+                    5, translate=(0.1, 0.1), scale=(0.9, 1.1), shear=5),
+                transforms.ColorJitter(brightness=0.1),
+                transforms.ToTensor(),
+                # transforms.Normalize(mean, std),
+            ])
+        else:
+            self.transform = transforms.Compose([
+                transforms.ToTensor(),
+                # transforms.Normalize(mean, std),
+            ])
+
+    def __getitem__(self, index):
+        # apply the transformations and return tensors
+        return self.transform(self.x_pil[index]), self.y_np[index]
+
+    def __len__(self):
+        return len(self.x_pil)
+
+
+def load_gtsrb_dataloader(data_dir, batch_size, num_workers=4):
+
+    x_train, y_train, x_val, y_val, x_test, y_test = load_gtsrb(
+        data_dir=data_dir)
+
+    # Standardization
+    mean = np.mean(x_train, (0, 1, 2))
+    std = np.std(x_train, (0, 1, 2))
+
+    trainset = GtsrbDataset(x_train, y_train, mean, std, augment=True)
+    validset = GtsrbDataset(x_val, y_val, mean, std, augment=False)
+    testset = GtsrbDataset(x_test, y_test, mean, std, augment=False)
+
+    trainloader = torch.utils.data.DataLoader(
+        trainset, batch_size=batch_size, shuffle=True, num_workers=num_workers)
+    validloader = torch.utils.data.DataLoader(
+        validset, batch_size=batch_size, shuffle=False, num_workers=num_workers)
+    testloader = torch.utils.data.DataLoader(
+        testset, batch_size=batch_size, shuffle=False, num_workers=num_workers)
+
+    return trainloader, validloader, testloader
---
+++ train_mnist.py
@@ -0,0 +1,156 @@
+'''Train MNIST model'''
+from __future__ import print_function
+
+import logging
+import os
+
+import torch
+import torch.backends.cudnn as cudnn
+import torch.nn as nn
+import torch.optim as optim
+
+import numpy as np
+from lib.dataset_utils import *
+from lib.mnist_model import *
+
+os.environ["CUDA_DEVICE_ORDER"] = "PCI_BUS_ID"
+os.environ["CUDA_VISIBLE_DEVICES"] = "1"
+
+
+def evaluate(net, dataloader, criterion, device, std=0):
+
+    net.eval()
+    val_loss = 0
+    val_correct = 0
+    val_total = 0
+    with torch.no_grad():
+        for batch_idx, (inputs, targets) in enumerate(dataloader):
+            inputs, targets = inputs.to(device), targets.to(device)
+            inputs += torch.randn_like(inputs) * std
+            outputs = net(inputs)
+            loss = criterion(outputs, targets)
+            val_loss += loss.item()
+            _, predicted = outputs.max(1)
+            val_total += targets.size(0)
+            val_correct += predicted.eq(targets).sum().item()
+
+    return val_loss / val_total, val_correct / val_total
+
+
+def train(net, trainloader, validloader, criterion, optimizer, epoch, device,
+          log, save_best_only=True, best_acc=0, model_path='./model.pt', std=0):
+
+    net.train()
+    train_loss = 0
+    train_correct = 0
+    train_total = 0
+    for batch_idx, (inputs, targets) in enumerate(trainloader):
+        inputs, targets = inputs.to(device), targets.to(device)
+        inputs += torch.randn_like(inputs) * std
+        optimizer.zero_grad()
+        outputs = net(inputs)
+        loss = criterion(outputs, targets)
+        loss.backward()
+        optimizer.step()
+
+        train_loss += loss.item()
+        _, predicted = outputs.max(1)
+        train_total += targets.size(0)
+        train_correct += predicted.eq(targets).sum().item()
+
+    val_loss, val_acc = evaluate(net, validloader, criterion, device, std=std)
+
+    log.info(' %5d | %.4f, %.4f | %8.4f, %7.4f', epoch,
+             train_loss / train_total, train_correct / train_total,
+             val_loss, val_acc)
+
+    # Save model weights
+    if not save_best_only or (save_best_only and val_acc > best_acc):
+        log.info('Saving model...')
+        torch.save(net.state_dict(), model_path)
+        best_acc = val_acc
+    return best_acc
+
+
+def main():
+
+    # Set experiment id
+    exp_id = 5
+    # model_name = 'train_mnist_exp%d' % exp_id
+    model_name = 'mnist_exp%d' % exp_id
+
+    # Training parameters
+    std = 2
+    batch_size = 128
+    epochs = 50
+    data_augmentation = False
+    learning_rate = 1e-3
+    l1_reg = 0
+    l2_reg = 0
+
+    # Subtracting pixel mean improves accuracy
+    subtract_pixel_mean = False
+
+    # Set all random seeds
+    seed = 2019
+    np.random.seed(seed)
+    torch.manual_seed(seed)
+
+    device = 'cuda' if torch.cuda.is_available() else 'cpu'
+
+    # Set up model directory
+    save_dir = os.path.join(os.getcwd(), 'saved_models')
+    if not os.path.isdir(save_dir):
+        os.makedirs(save_dir)
+    model_path = os.path.join(save_dir, model_name + '.h5')
+
+    # Get logger
+    log_file = model_name + '.log'
+    log = logging.getLogger('train_mnist')
+    log.setLevel(logging.DEBUG)
+    # Create formatter and add it to the handlers
+    formatter = logging.Formatter(
+        '[%(levelname)s %(asctime)s %(name)s] %(message)s')
+    # Create file handler
+    fh = logging.FileHandler(log_file, mode='w')
+    fh.setLevel(logging.DEBUG)
+    fh.setFormatter(formatter)
+    log.addHandler(fh)
+
+    log.info(log_file)
+    log.info(('MNIST | exp_id: {}, seed: {}, init_learning_rate: {}, ' +
+              'batch_size: {}, l2_reg: {}, l1_reg: {}, epochs: {}, ' +
+              'data_augmentation: {}, subtract_pixel_mean: {}').format(
+                  exp_id, seed, learning_rate, batch_size, l2_reg, l1_reg,
+                  epochs, data_augmentation, subtract_pixel_mean))
+
+    log.info('Preparing data...')
+    trainloader, validloader, testloader = load_mnist(
+        batch_size, data_dir='/data', val_size=0.1, shuffle=True, seed=seed)
+
+    log.info('Building model...')
+    net = BasicModel()
+    net.to(device)
+    # if device == 'cuda':
+    #     net = torch.nn.DataParallel(net)
+    #     cudnn.benchmark = True
+
+    criterion = nn.CrossEntropyLoss()
+    optimizer = optim.Adam(net.parameters(), lr=learning_rate)
+
+    log.info(' epoch | loss  , acc    | val_loss, val_acc')
+    best_acc = 0
+    for epoch in range(epochs):
+        best_acc = train(net, trainloader, validloader, criterion, optimizer,
+                         epoch, device, log, save_best_only=True,
+                         best_acc=best_acc, model_path=model_path, std=std)
+
+    test_loss, test_acc = evaluate(net, testloader, criterion, device, std=std)
+    log.info('Test loss w/ noise: %.4f, Test acc w/ noise: %.4f',
+             test_loss, test_acc)
+    test_loss, test_acc = evaluate(net, testloader, criterion, device, std=0)
+    log.info('Test loss: %.4f, Test acc: %.4f', test_loss, test_acc)
+
+
+if __name__ == '__main__':
+    main()
